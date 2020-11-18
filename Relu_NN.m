535438a172d5db158be48647933a56010cf9a9d3
mtv3673crawlab <minhvu0194@gmail.com>
2020-11-18 12:09:52 -0600

    Retest
    Fix convex regularizer cost

---
+++ Relu_NN.m
@@ -0,0 +1,71 @@
+clear
+clc
+close all
+
+% training data 
+X = [-2 1;
+     -1 1;
+      0 1;
+      1 1;
+      2 1];
+Y = [1;-1;1;1;-1];
+
+% load('data.mat','X','Y')
+
+
+n = size(X,1); 
+m = 8; % number of hidden neurons 
+beta = 0.0002;
+lr = 0.01;
+
+ReLU=@(x) (abs(x)+x)/2;
+
+
+% initialize weights
+w1 = rand(size(X,2),m);
+w2 = rand(m,1);
+store_cost = [];
+
+
+for i=1:5000
+% Forward
+layer1 = X*w1;
+layer1_ReLU = ReLU(layer1);  
+output = layer1_ReLU*w2;
+
+cost = 0.5*norm(output - Y)^2 + 0.5*beta*(norm(w1,'fro')^2 + norm(w2)^2);
+store_cost = [store_cost, cost];
+
+% Backprop
+grad_loss = (output - Y);
+grad_w2 = layer1_ReLU'*grad_loss + beta*(w2);
+grad_layer1 = grad_loss*w2';
+grad_layer1_ReLU =  gradReLU(grad_layer1,layer1);
+grad_w1 = X'*(grad_layer1_ReLU) + beta*w1;
+
+w1 = w1 - lr*grad_w1;
+w2 = w2 - lr*grad_w2;
+
+end
+
+plot(store_cost,'LineWidth',2)
+hold on
+axis([-100 5000 -0.1 4])
+
+
+function x = gradReLU(x,y) 
+x(find(y<0))=0;
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ convex_NN.m
@@ -0,0 +1,108 @@
+clear
+clc
+close all
+
+% training data 
+X = [-2 1;
+     -1 1;
+      0 1;
+      1 1;
+      2 1];
+Y = [1;-1;1;1;-1];
+
+n = size(X,1); 
+
+% Generate random u from a uniform distribution  
+U=2*(rand(2,100)-0.5);   %scatter(U(1,:),U(2,:))
+
+% For each u, compute a diagonal vector 
+store_diag_vec = [];
+for i=1:size(U,2)
+    diag_vec = zeros(1,size(X,1));
+    indicator = find(X*U(:,i)>=0);
+    diag_vec(indicator) = 1;    
+    
+    if any(diag_vec)==0
+        continue
+    end
+    
+    % Check if a diagonal vector already exist
+    if  isempty(store_diag_vec) 
+        store_diag_vec = [store_diag_vec; diag_vec];
+    elseif ~ismember(diag_vec,store_diag_vec,'rows')
+        store_diag_vec = [store_diag_vec; diag_vec];
+    end
+end
+store_diag_vec
+
+
+% Form diagonal matrices Di
+P = size(store_diag_vec,1);
+for i=1:P
+    D(:,:,i)=diag(store_diag_vec(i,:));
+end
+D
+
+
+%% Solve the regularized convex problem (8)
+beta = 0.0002;
+
+cvx_begin %quiet
+    variable v(2,P) 
+    variable w(2,P)
+    obj = 0;
+    regulizer = 0;
+    for i = 1:P
+        obj = obj + D(:,:,i)*X*(v(:,i)-w(:,i));
+        regulizer = regulizer + norm(v(:,i)) + norm(w(:,i));
+    end
+    obj = 0.5*norm(obj-Y)^2;
+    
+    minimize(obj+beta*regulizer);
+    subject to
+        for i = 1:P
+            (2*D(:,:,i)-eye(n))*X*v(:,i)>=0;
+            (2*D(:,:,i)-eye(n))*X*w(:,i)>=0;
+        end
+cvx_end
+
+%% Compute optimal weights 
+W1 = [];
+W2 = [];
+for i = 1:P
+    if norm(v(:,i)) > 1e-3
+        W1 = [W1, v(:,i)/norm(v(:,i))];
+        W2 = [W2; norm(v(:,i))];
+    end
+end
+for i = 1:P
+    if norm(w(:,i)) > 1e-3
+        W1 = [W1, w(:,i)/norm(w(:,i))];
+        W2 = [W2; norm(w(:,i))];
+    end
+end
+
+    
+ReLU=@(x) (abs(x)+x)/2;
+
+layer1 = X*W1;
+layer1_ReLU = ReLU(layer1);  
+output = layer1_ReLU*W2;
+
+cost = 0.5*norm(output - Y)^2 + 0.5*beta*(norm(W1,'fro')^2 + norm(W2)^2)
+
+%%
+0.5*beta*(norm(W1,'fro')^2 + norm(W2)^2)
+
+
+
+
+
+
+
+
+
+
+
+
+
